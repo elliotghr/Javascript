8ee32213d5bac7f2779ac21f563a98d38ad1eb83
elliotghr <gunsnroses.2617@gmail.com>
2023-02-08 19:07:30 -0600

    Comentarios añadidos al video_inteligente

--- DOM-Ejercicios/video_inteligente.js
+++ DOM-Ejercicios/video_inteligente.js
@@ -4,15 +4,22 @@ const d = document,
 export default function smartVideo() {
   const $videos = d.querySelectorAll("video[data-smart-video]");
 
-  const cb = (entries) => {
+  // Esta función recibe las entries dadas por el IntersectionObserver
+  const callback = (entries) => {
     // console.log("entries", entries); //podemos ver las propiedades de cada entrada
     entries.forEach((entry) => {
+      // la propiedad isIntersecting nos ayuda a saber en que momento se cumple la condición del limite y con ella desencadenamos nuestra lógica
       if (entry.isIntersecting) {
         entry.target.play();
       } else {
         entry.target.pause();
       }
+
+      // visibilitychange
+      // El evento visibilitychange se activa en el documento cuando el contenido de su pestaña se vuelve visible o se oculta.
+
       w.addEventListener("visibilitychange", (e) => {
+        // visibilityState nos dice si la pagina está en estado hidden o visible
         if (d.visibilityState === "visible") {
           entry.target.play();
         } else {
@@ -21,8 +28,9 @@ export default function smartVideo() {
       });
     });
   };
-
-  const observer = new IntersectionObserver(cb, { threshold: 0.5 });
-
+  // doc -> https://developer.mozilla.org/es/docs/Web/API/Intersection_Observer_API
+  // Se asigna la función a ejecutar cuando se cumpla el limite y se asigna el limite como un objeto
+  const observer = new IntersectionObserver(callback, { threshold: 0.5 });
+  // Se asignan los elementos a observar
   $videos.forEach((element) => observer.observe(element));
 }
