ec8d11a1acb4c4cac59787a0fda69734c1101724
elliotghr <gunsnroses.2617@gmail.com>
2023-02-09 12:18:31 -0600

    Comentarios y función autoplay agregados

--- DOM-Ejercicios/carrusel.js
+++ DOM-Ejercicios/carrusel.js
@@ -6,22 +6,45 @@ export default function slider() {
     $slides = d.querySelectorAll(".slider-slide");
 
   let i = 0; //contador
+  let autoPlayInterval; // se crea la variable para almacenar el contador
+
+  // función para crear el auto play al slider
+  const autoPlay = () => {
+    autoPlayInterval = setInterval(() => {
+      $slides[i].classList.remove("active");
+      i++;
+      if (i > $slides.length - 1) {
+        i = 0;
+      }
+      $slides[i].classList.add("active");
+    }, 2000);
+  };
+  // función para detener el comportamiento automatico del slider
+  const autoStop = () => {
+    clearInterval(autoPlayInterval);
+  };
+  autoPlay();
+
   d.addEventListener("click", (e) => {
     if (e.target === $prevBtn) {
+      // Prevenimos la acción x defecto del elemento a
       e.preventDefault();
+      autoStop();
+      // removemos la clase active
       $slides[i].classList.remove("active");
       i--;
       // console.log($slides);
-
+      // Si es menor a 0 entonces i se coloca en la última slide
       if (i < 0) {
         i = $slides.length - 1;
       }
-
+      // a esa slide se le asigna la clase active
       $slides[i].classList.add("active");
     }
 
     if (e.target === $nxtBtn) {
       e.preventDefault();
+      autoStop();
       $slides[i].classList.remove("active");
       i++;
       //   console.log($slides);
